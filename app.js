e624830e70c1f17a4811d09949bcdfa48d17d688
Jorge <jorgecortesing@gmail.comgit config --global user.email jorgecortesing@gmail.comgit config --global user.email jorgecortesing@gmail.com>
2022-05-12 16:51:02 -0500

    Agregando segunda fase - estilos

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-const express = require('express');
-const res = require('express/lib/response');
+const express = require("express");
 const app = express();
 
-const path = require('path');
+const path = require("path");
 
 const port = 4000;
 
-
 // Utilizar la carpeta public
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //rutas
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/home.html'))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
-
 // Servidor
-app.listen(port, ()=>{
-    console.log(`Server on port ${port}`)
+app.listen(port, () => {
+  console.log(`Server on port ${port}`);
 });
