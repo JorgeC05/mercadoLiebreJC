a856165fc29b52a72fe3576377349229e2e3efc8
Jorge <jorgecortesing@gmail.comgit config --global user.email jorgecortesing@gmail.comgit config --global user.email jorgecortesing@gmail.com>
2022-05-19 19:28:29 -0500

    Agregando quinta fase - Formularios

--- app.js
+++ app.js
@@ -13,8 +13,20 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
+});
+
+app.post("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+
 // Servidor
 app.listen(port, () => {
   console.log(`Server on port ${port}`);
 });
-
